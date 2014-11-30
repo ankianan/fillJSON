4036681a2ea2eeabeb113edcde4dc8136bce1a16
ankit.anand <ankit.anand@naukri.com>
2014-11-30 16:12:04 +0530

    filljson.js modified

--- filljson.js
+++ filljson.js
@@ -1,24 +1,19 @@
 var struct_proto = {
-    /** Common set of functions avilable to every node, you can extend it any time */
+    /** Common set of functions avilable to every structure, you can extend it any time */
     foo: function (node, key, value) {
-        var _value = this['_' + key];
-        if (_value) {
-            node = node.find(_value);
-        }
-        this[key].call(node, value);
-    },
-        'html': function (value) {
-        this.html(value);
-    },
-        'class': function (value) {
-        this.addClass(value);
-    },
-        'href': function (value) {
-        this.attr('href', value);
-    },
-        'title': function (value) {
-        this.attr('title',value);
-    },
+		//value may not be string in case of key "attr"
+		if(key == 'attr'){
+			this[key](value,node);
+		}
+		else{
+			var selector = this['_' + key];
+			if (selector) {
+				node = node.find(selector);
+			}
+			this[key].call(node, value);
+		}
+        
+    },        
     
     /** Function to manage Structure */
     getCont: function () {
@@ -33,7 +28,30 @@ var struct_proto = {
 
             for (key in b) {
                 if (key == 'list') {
-                    node.append(_this.menu.init(b[key]))
+					
+					var _arr = b['list'];
+					var _struct,subMenu;						
+					
+					if(_this.menu.struct){
+						_struct = _this.menu.struct;					
+						subMenu_cont = _this.menu.html.clone();
+						node.append(subMenu_cont);						
+					}
+					else{
+						_struct = _this.menu;
+						subMenu_cont = node;		
+					}
+						
+					if ($.type(_arr[0]) == 'array') {											
+						$.each(_arr||[], function (c, nodeArr) {
+							subMenu_cont.addClass('c'+_arr.length)	
+							subMenu_cont.append(_struct.init(nodeArr));
+						});
+					} 
+					else{
+						subMenu_cont.append(_struct.init(_arr));
+					}					
+					
 
                 } else if (key in _this) {
                     _this.foo(node, key, b[key]);
@@ -46,7 +64,7 @@ var struct_proto = {
 }
 
 var struct = function (obj) {
-    for (key in obj)
-    this[key] = obj[key]
+    $.extend(true,this,obj);
 }
+	
 struct.prototype = struct_proto;
